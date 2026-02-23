730022cbabcce7962aebeefcfc3f28affaf704ed
PRAVESH TIWARI <pravesht252@gmail.com>
2026-02-23 12:39:24 +0530

    Initial commit

---
+++ firstday.java
@@ -0,0 +1,51 @@
+public class firstday {
+    public static void main(String[] args) {
+        //Print the range
+
+        System.out.println(Byte.MIN_VALUE+" "+Byte.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE+" "+Short.MAX_VALUE);
+
+        System.out.println(Integer.MIN_VALUE+" "+Integer.MAX_VALUE);
+        System.out.println( Long.MIN_VALUE+" "+Long.MAX_VALUE);   
+        
+        
+        // type casting
+
+        int num = 10;
+        double d = num;   
+        
+        System.out.println(d);
+
+
+        // Airthmetic operator
+
+        int c=30, e=3;
+
+        System.out.println(c+e);
+        System.out.println(c-e);
+        System.out.println(c*e);
+        System.out.println(c/e);
+
+
+        //Assignment operator
+
+        int assign=10;
+        assign+=5;
+        System.out.println(assign);
+
+        //Relation operator
+
+        int a=100, b=20;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+        //logical operator
+
+        int b1=5;
+        System.out.println((a>b) && b1 <10);
+        System.out.println((a>b) || b1 >10);
+
+    }
+}
