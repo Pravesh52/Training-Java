f5f72df0fb3b0cfa89d554901b2fd34a248654d3
PRAVESH TIWARI <pravesht252@gmail.com>
2026-02-24 22:21:08 +0530

    Training

---
+++ Secondclass/Second.java
@@ -0,0 +1,25 @@
+package Secondclass;
+
+public class Second {
+    public static void main(String[] args) {
+        
+
+        //MATRIX
+
+        //2D MATRIX
+
+        
+
+        //MULTIPLICATION MATRIX
+
+
+
+        //CHECK THE NUMBER IS HAPPY, STRONG 
+
+
+
+
+
+    }
+    
+}
